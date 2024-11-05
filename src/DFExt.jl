f7f87c95aeaf31cc0b9c1ed29db9cd85e93180a6
Paco Ho <kaiipho@gmail.com>
2024-11-05 10:45:51 +0800

    add htmltables func

--- src/DFExt.jl
+++ src/DFExt.jl
@@ -21,7 +21,7 @@ export readExcel, toExcel
 export toexcel
 
 export read_jld2, to_jld2, readJLD2, toJLD2
-export htmlTables
+export htmlTables, htmltables
 export readexcel
 
 ffill(v) = v[accumulate(max, [i*!ismissing(v[i]) for i in 1:length(v)], init=1)]
@@ -68,6 +68,41 @@ function htmlTables(html::HTMLElement; selector::AbstractString="", startRow=1):
     dfs
 end
 
+
+function htmltables(html::HTMLElement; selector::AbstractString="", startRow=1)::Vector{AbstractDataFrame}
+    function fillMissingHeaders(headers)
+        count = 1
+        for (index, value) in enumerate(headers)
+            if value == ""
+                headers[index] = "missing_"*string(count)
+                count += 1
+            end
+        end
+    end
+    
+    tables = eachmatch(Selector("table$(selector)"), html)
+    dfs = DataFrame[]
+    for table in tables
+        tblrows = eachmatch(sel"tr", table)
+        tblheaders = try
+            text.(tblrows[startRow] |> children)
+        catch
+            continue
+        end
+        
+        fillMissingHeaders(tblheaders)
+        df = DataFrame(map(th-> th => [], tblheaders), makeunique=true) # create emtpy dataframe with colnames
+        
+        for tblrow in tblrows[startRow+1:end]
+            tbldata = text.(tblrow |> children)
+            if length(tbldata) == length(tblheaders)
+                push!(df, tbldata)
+            end
+        end
+        push!(dfs, df)
+    end
+    dfs
+end
 
 # function readHTML(response::HTTP.Messages.Response; selector::AbstractString="", startRow=1)::Vector{AbstractDataFrame}
 #     # content = response_content(response, charset(response))
